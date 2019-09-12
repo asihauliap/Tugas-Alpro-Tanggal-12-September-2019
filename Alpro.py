493ab97dabcad41ff8a3f6fdf5fb5a95a94a1603
asihauliap <54665745+asihauliap@users.noreply.github.com>
2019-09-12 09:45:23 +0800

    Tugas Asih Aulia Putri-TI-42-11-1201184070
    Tugas Asih Aulia Putri tentang Looping, Array, If else condition

---
+++ Alpro.py
@@ -0,0 +1,17 @@
+print("Start")
+
+GPA = [3, 2.7, 2.5, 4]
+
+def Bonus():
+    TotalBonus = x * A
+    return TotalBonus
+
+A = 500000
+
+for x in GPA :
+    print(x)
+    if x > 3:
+        print(x,"Anda mendapatkan bonus Rp. ", Bonus())
+    elif x <= 3:
+        print("Anda tidak mendapatkan bonus")
+
